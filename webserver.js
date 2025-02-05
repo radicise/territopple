72dfb92f469e3c4dc640af8d5379f0b6b8dae446
Tristan <tschartman41@gmail.com>
2025-02-05 11:35:16 -0500

    favicon

--- webserver.js
+++ webserver.js
@@ -4,6 +4,7 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
+// const { pipeline } = require("stream/promises");
 
 const settings = JSON.parse(fs.readFileSync(path.join(__dirname, "settings.json"), {encoding:"utf-8"}));
 
