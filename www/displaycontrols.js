2583f93ff6d0b500966466e3c6a416cdacfa4c83
Vihaan Jim <vsj314@icloud.com>
2025-05-30 14:39:32 -0400

    Use standardized last move selector

--- www/displaycontrols.js
+++ www/displaycontrols.js
@@ -7,10 +7,6 @@ const extraSettings = document.getElementById("extra-display-settings");
 /**@type {HTMLInputElement} */
 const settingsCheckbox = document.getElementById("settings-expand");
 
-/**@type {HTMLInputElement} */
-const lastMoveCheckbox = document.getElementById("last-move-enabled");
-
-
 function setTileSize() {
     container.style.setProperty("--tile-font-size", slider.value);
     window.dispatchEvent(new CustomEvent("gameboard-fresize"));
@@ -26,15 +22,6 @@ settingsCheckbox.addEventListener("change", () => {
         extraSettings.style.display = "none";
     }
 });
-
-lastMoveCheckbox.addEventListener("change", () => {
-    displaySettings.highlightLastMove = lastMoveCheckbox.checked;
-    // if (lastMoveCheckbox.checked) {
-    //     container.style.setProperty("--tile-last-move", lastMoveColorPicker.value);
-    // } else {
-    //     container.style.setProperty("--tile-last-move", "#00000000");
-    // }
-})
 
 class TTColorPicker extends HTMLElement {
     static observedAttributes = ["var", "desc", "default"];
@@ -92,20 +79,3 @@ class TTStyleToggle extends HTMLElement {
     }
 }
 customElements.define("x-style-toggle", TTStyleToggle);
-
-// for (const [picker, checker, valueName, styleName] of [[hoverColorPicker, hoverCheckbox, "hoverColor", "--tile-hover"], [lastMoveColorPicker, lastMoveCheckbox, "lastMoveColor", "--tile-last-move"], [volatileColorPicker, volatileCheckbox, "volatileColor", "--tile-volatile"]]) {
-//     const value = localStorage.getItem(valueName);
-//     if (Boolean(value)) {
-//         picker.value = value;
-//         container.style.setProperty(styleName, value+"1f");
-//     } else if (value === "false") {
-//         checker.checked = false;
-//         switch (valueName) {
-//             case "hoverColor":container.classList.remove("tile-hover");break;
-//             case "lastMoveColor":displaySettings.highlightLastMove = false;break;
-//             case "volatileColor":container.classList.remove("volatiles");break;
-//         }
-//     } else {
-//         localStorage.setItem(valueName, picker.value);
-//     }
-// }
