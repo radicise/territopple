40e3bca3776e250cb30c5302c6ff1197b6eef37b
Vihaan Jim <vsj314@icloud.com>
2025-05-30 13:47:46 -0400

    Possibly working simplified customization controls

--- www/displaycontrols.js
+++ www/displaycontrols.js
@@ -8,19 +8,7 @@ const extraSettings = document.getElementById("extra-display-settings");
 const settingsCheckbox = document.getElementById("settings-expand");
 
 /**@type {HTMLInputElement} */
-const hoverCheckbox = document.getElementById("hover-enabled");
-/**@type {HTMLInputElement} */
-const hoverColorPicker = document.getElementById("hover-color");
-
-/**@type {HTMLInputElement} */
 const lastMoveCheckbox = document.getElementById("last-move-enabled");
-/**@type {HTMLInputElement} */
-const lastMoveColorPicker = document.getElementById("last-move-color");
-
-/**@type {HTMLInputElement} */
-const volatileCheckbox = document.getElementById("volatiles-enabled");
-/**@type {HTMLInputElement} */
-const volatileColorPicker = document.getElementById("volatiles-color");
 
 
 function setTileSize() {
@@ -31,47 +19,12 @@ slider.addEventListener("change", () => {
     window.dispatchEvent(new CustomEvent('gameboard-fresize'));
 });
 
-// slider.addEventListener("input", () => {
-// });
-// document.addEventListener("keydown", (e) => {
-//     switch (e.code) {
-//     case "Minus":
-//         let zoom = Math.min(Math.max(parseInt(container.style.getProperty("--tile-font-size")) - 1, 10), 100);
-//         container.style.setProperty("--tile-font-size", zoom);
-//         slider.value = zoom;
-//         break;
-//     case "Equal":
-//         let zoom = Math.min(Math.max(parseInt(container.style.getProperty("--tile-font-size")) + 1, 10), 100);
-//         container.style.setProperty("--tile-font-size", zoom);
-//         slider.value = zoom;
-//         break;
-//     }
-// });
-
 settingsCheckbox.addEventListener("change", () => {
     if (settingsCheckbox.checked) {
         extraSettings.style.display = "block";
     } else {
         extraSettings.style.display = "none";
     }
-});
-
-hoverCheckbox.addEventListener("change", () => {
-    if (hoverCheckbox.checked) {
-        container.classList.add("tile-hover");
-    } else {
-        container.classList.remove("tile-hover");
-    }
-    // if (hoverCheckbox.checked) {
-    //     container.style.setProperty("--tile-hover", hoverColorPicker.value+"1f");
-    // } else {
-    //     container.style.setProperty("--tile-hover", "#00000000");
-    // }
-})
-
-hoverColorPicker.addEventListener("change", () => {
-    container.style.setProperty("--tile-hover", hoverColorPicker.value+"1f");
-    localStorage.setItem("hoverColor", hoverColorPicker.value);
 });
 
 lastMoveCheckbox.addEventListener("change", () => {
@@ -83,24 +36,62 @@ lastMoveCheckbox.addEventListener("change", () => {
     // }
 })
 
-lastMoveColorPicker.addEventListener("change", () => {
-    container.style.setProperty("--tile-last-move", lastMoveColorPicker.value+"1f");
-    localStorage.setItem("lastMoveColor", lastMoveColorPicker.value);
-});
-
-volatileCheckbox.addEventListener("change", () => {
-    if (volatileCheckbox.checked) {
-        container.classList.add("volatiles");
-    } else {
-        container.classList.remove("volatiles");
+class TTColorPicker extends HTMLElement {
+    static observedAttributes = ["var", "desc", "default"];
+    
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
     }
-    // container.style.setProperty("--tile-volatile", )
-});
 
-volatileColorPicker.addEventListener("change", () => {
-    container.style.setProperty("--tile-volatile", volatileColorPicker.value+"1f");
-    localStorage.setItem("volatileColor", volatileColorPicker.value);
-});
+    connectedCallback() {
+        let v = this.getAttribute("var") || "";
+        this.shadowRoot.innerHTML = `
+	    <label for="${v}-color">${this.getAttribute("desc")}</label>
+	    <input type="color" id="${v}-color" value="${this.getAttribute('default') || '#000000'}">
+	`;
+        let colorPicker = this.shadowRoot.getElementById(`${v}-color`);
+        colorPicker.addEventListener("change", () => {
+            container.style.setProperty(`--{v}`, colorPicker.value+"1f");
+            localStorage.setItem(v, colorPicker.value);
+        });
+        let p = localStorage.getItem(v);
+        if (p) {
+            container.style.setProperty(`--{v}`, p);
+        } else if (this.hasProperty("default")) {
+            container.style.setProperty(`--{v}`, this.getAttribute("default"));
+        }
+    }
+}
+customElements.define("x-color-picker", TTColorPicker);
+
+class TTStyleToggle extends HTMLElement {
+    static observedAttributes = ["var", "default"];
+    
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        let v = this.getAttribute("var") || "";
+        this.shadowRoot.innerHTML = `
+	    <input type="checkbox" id="${v}-enabled" checked="${this.getAttribute('default') || true}">
+	`;
+        let checkbox = this.shadowRoot.getElementById(`${v}-color`);
+        checkbox.addEventListener("change", () => {
+            if (checkbox.checked) {
+                container.classList.add(v);
+            } else {
+                container.classList.remove(v);
+            }
+        });
+        if (this.getAttribute("default") == false) {
+            container.classList.remove(v);
+        }
+    }
+}
+customElements.define("x-style-toggle", TTStyleToggle);
 
 for (const [picker, checker, valueName, styleName] of [[hoverColorPicker, hoverCheckbox, "hoverColor", "--tile-hover"], [lastMoveColorPicker, lastMoveCheckbox, "lastMoveColor", "--tile-last-move"], [volatileColorPicker, volatileCheckbox, "volatileColor", "--tile-volatile"]]) {
     const value = localStorage.getItem(valueName);
