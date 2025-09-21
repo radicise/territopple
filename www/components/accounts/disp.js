2c27e100ec401d5301403c87c59b84a442c4afdb
Tristan <73963573+tsj845@users.noreply.github.com>
2025-09-20 22:11:08 -0400

    text

--- www/components/accounts/disp.js
+++ www/components/accounts/disp.js
@@ -1,7 +1,7 @@
 fetch(`https://${document.location.hostname}/acc/pub/%40self/info`).then(v => {
     if (v.status === 200) {
         v.json().then(j => {
-            document.getElementById("account-name").children[0].textContent = j.name;
+            document.getElementById("account-name").children[0].textContent = `${j.name} (profile)`;
             document.getElementById("account-name").children[0].href = "/account/info";
         });
     }
