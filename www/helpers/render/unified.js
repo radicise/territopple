4d7b49da7091657adb7ea6b5d4081c667563f0b4
Tristan <73963573+tsj845@users.noreply.github.com>
2025-09-26 19:40:44 -0400

    Update unified.js

--- www/helpers/render/unified.js
+++ www/helpers/render/unified.js
@@ -132,3 +132,45 @@ let {updateTile, createBoard, setVolatile, flushUpdates} = (()=>{
         document.body.appendChild(s);
     }
 }
+
+let __runi_board;
+let __runi_teamboard;
+let __runi_topo;
+
+/**
+ * @param {Topology} topo
+ * @param {ArrayLike<number>} board
+ * @param {ArrayLike<number>} teamboard
+ */
+function setup(topo, board, teamboard) {
+    /**@type {HTMLSelectElement} */
+    const bro = document.getElementById("board-rendering-option");
+    __runi_topo = topo;
+    __runi_board = board;
+    __runi_teamboard = teamboard;
+    createBoard(topo, board, teamboard, Number(bro.value)-1);
+    flushUpdates();
+    bro.onchange = () => {
+        createBoard(topo, board, teamboard, Number(bro.value)-1);
+        flushUpdates();
+        document.getElementById("spherical-bloom-enabled").hidden = (bro.value !== "3");
+    };
+    document.getElementById("spherical-enable-bloom").onchange = () => {
+        window.postMessage({type:"3d-setbloom",enabled:document.getElementById("spherical-enable-bloom").checked});
+    };
+}
+
+/**
+ * @param {number[]} oldb
+ * @param {number[]} oldt
+ */
+function updateBoard(oldb, oldt) {
+    for (let i = 0,l=__runi_topo.tileCount; i < l; i ++) {
+        if ((oldb[i] !== __runi_board[i]) || (oldt[i] !== __runi_teamboard[i])) {
+            const p = this.topology.getPositionOf(i, "2d-grid");
+            updateTile(p, __runi_teamboard[i], __runi_board[i]);
+            setVolatile(p, __runi_board[i] === __runi_topo.getNeighbors(i).length);
+        }
+    }
+    flushUpdates();
+}
