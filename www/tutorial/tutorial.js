8c3bad4c90851f0ab9213492e95f341395cfc6dd
Tristan <73963573+tsj845@users.noreply.github.com>
2025-11-17 13:50:38 -0500

    Update tutorial.js

--- www/tutorial/tutorial.js
+++ www/tutorial/tutorial.js
@@ -64,7 +64,7 @@ const info = {
             p.textContent = item.t;
             messageArea.appendChild(p);
             p.scrollIntoView({"behavior":"smooth"});
-            messageArea.scrollBy({"behavior":"smooth","top":50});
+            messageArea.scrollBy({"behavior":"smooth","top":-50});
         }
         if (item.m) {
             doMove(item.m[2], item.m[0]*5+item.m[1]);
