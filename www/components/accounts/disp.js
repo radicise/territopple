ce75da2f5977c90967bb6221c169ba8cb4863c6a
Tristan <73963573+tsj845@users.noreply.github.com>
2025-11-18 20:42:12 -0500

    update how account login link is shown

--- www/components/accounts/disp.js
+++ www/components/accounts/disp.js
@@ -1,8 +1,9 @@
 fetch(`https://${document.location.hostname}/acc/pub/%40self/info`).then(v => {
     if (v.status === 200) {
         v.json().then(j => {
-            document.getElementById("account-name").children[0].textContent = `${j.name} (profile)`;
-            document.getElementById("account-name").children[0].href = "/account/info";
+            document.getElementById("account-name").children[0].textContent = `${j.name} `;
+            document.getElementById("account-name").children[1].children[0].textContent = "profile";
+            document.getElementById("account-name").children[1].children[0].href = "/account/info";
         });
     }
 });
