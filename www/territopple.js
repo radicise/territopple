ef76073414a15e34934f3e95cbe126b9e16b26b7
Tristan <73963573+tsj845@users.noreply.github.com>
2025-12-12 21:19:36 -0500

    custom team colors

--- www/territopple.js
+++ www/territopple.js
@@ -388,6 +388,10 @@ conn.addEventListener("open", async function(event) {
                 createBanner({type:"info",content:`Game started`});
                 break;
             }
+            case "waiting:teamcols":{
+                setColors(data.payload["c"].map(v=>"#"+([v>>16,(v>>8)&0xff,v&0xff].map(vi=>vi.toString(16)).join())),game.topology,game.teamboard);
+                break;
+            }
             case "game:kick":{
                 let n = data.payload["n"];
                 if (typeof n === "number") {
@@ -541,6 +545,7 @@ conn.addEventListener("open", async function(event) {
                 ifmt.turn = 0;
                 // await game.setConfig(topology.m.formatDimensions(dims), players);
                 await game.setConfig(dims, players);
+                setColors(data.payload["c"].map(v=>"#"+([v>>16,(v>>8)&0xff,v&0xff].map(vi=>vi.toString(16)).join())),game.topology,game.teamboard);
                 updScr("status", `${game.joinedPlayers} player(s) present in room, ${game.maxPlayers} players max`);
                 if (configed) {
                     configed();
