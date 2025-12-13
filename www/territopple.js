5214b493a6eef6f5a70339f6407696c6b8982fa2
Tristan <73963573+tsj845@users.noreply.github.com>
2025-12-12 21:22:07 -0500

    Update territopple.js

--- www/territopple.js
+++ www/territopple.js
@@ -546,6 +546,7 @@ conn.addEventListener("open", async function(event) {
                 // await game.setConfig(topology.m.formatDimensions(dims), players);
                 await game.setConfig(dims, players);
                 if (data.payload["c"]) {
+                    console.log(data.payload["c"]);
                     setColors(data.payload["c"].map(v=>"#"+([v>>16,(v>>8)&0xff,v&0xff].map(vi=>vi.toString(16)).join())),game.topology,game.teamboard);
                 }
                 updScr("status", `${game.joinedPlayers} player(s) present in room, ${game.maxPlayers} players max`);
