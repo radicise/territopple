7c1c606d6857b3b0ce4603a1efd853f8e3b5f765
Tristan <73963573+tsj845@users.noreply.github.com>
2026-01-15 20:07:49 -0500

    Update make.js

--- www/components/utils/make.js
+++ www/components/utils/make.js
@@ -86,7 +86,7 @@ const make = (nodename, attrs) => {
         e.oninput = (ev)=>{attrs.oninput(e,ev);};
     }
     if (attrs?.readonly) {
-        e.readonly = attrs.readOnly;
+        e.readOnly = attrs.readonly;
     }
     return e;
 };
