051942a033c4524cc6ab910f6612b9bfd1bf46d3
Tristan <73963573+tsj845@users.noreply.github.com>
2026-02-13 22:38:09 -0500

    Update waiting.js

--- zserver/socks/waiting.js
+++ zserver/socks/waiting.js
@@ -88,8 +88,8 @@ const handler = (sock, globals, {change, emit, onall, on}, args, state) => {
         }
     });
     onall("account:found", (data) => {
-        console.log(data["n"]);
-        console.log(state.game?.ident);
+        // console.log(data["n"]);
+        // console.log(state.game?.ident);
         let d = true;
         if (typeof state.playerNum === "number" && data["n"] === state.playerNum) {
             state.game.players[state.playerNum].accId = data["a"];
