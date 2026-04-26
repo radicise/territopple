cbb55a6a694ccf3613bb992e5ac46a6a23466016
Tristan <73963573+tsj845@users.noreply.github.com>
2026-04-26 15:48:24 -0400

    pilot

--- www/components/accounts/disp.js
+++ www/components/accounts/disp.js
@@ -1,23 +1,27 @@
-fetch(`https://${document.location.hostname}/acc/pub/%40self/info`).then(v => {
-    const tar = document.getElementById("account-name").children[1];
-    if (v.status === 200) {
-        v.json().then(j => {
-            tar.children[1].textContent = `${j.name} `;
-            tar.children[2].children[0].textContent = "profile";
-            tar.children[2].children[0].href = "/account/info";
-        });
-    } else {
-        tar.children[1].textContent = "Guest ";
-        tar.children[2].children[0].textContent = "login";
-    }
-});
-fetch(`https://${document.location.hostname}/acc/pfp/info/%40self`).then(v => {
-    /**@type {HTMLImageElement} */
-    const tar = document.getElementById("account-name").children[1].children[0];
-    if (v.status === 200) {
-        tar.hidden = false;
-    }
-});
+if (document.location.pathname.startsWith("/account")) {
+    document.getElementById("account-name").children[1].hidden = true;
+} else {
+    fetch(`https://${document.location.hostname}/acc/pub/%40self/info`).then(v => {
+        const tar = document.getElementById("account-name").children[1];
+        if (v.status === 200) {
+            v.json().then(j => {
+                tar.children[1].textContent = `${j.name} `;
+                tar.children[2].children[0].textContent = "profile";
+                tar.children[2].children[0].href = "/account/info";
+            });
+        } else {
+            tar.children[1].textContent = "Guest ";
+            tar.children[2].children[0].textContent = "login";
+        }
+    });
+    fetch(`https://${document.location.hostname}/acc/pfp/info/%40self`).then(v => {
+        /**@type {HTMLImageElement} */
+        const tar = document.getElementById("account-name").children[1].children[0];
+        if (v.status === 200) {
+            tar.hidden = false;
+        }
+    });
+}
 document.getElementById("account-name").children[0].children[1].addEventListener("change", function () {
     document.body.parentElement.style.setProperty("--theme", this.value);
 });
