1c425a004c43c8efd6bd1229cf0f7d575aea2334
Tristan <73963573+tsj845@users.noreply.github.com>
2026-04-28 23:13:32 -0400

    pretty

--- www/community/community.js
+++ www/community/community.js
@@ -102,11 +102,12 @@ async function loadPage(search, page) {
     for (const entry of data) {
         const row = document.createElement("tr");
         const pfp = document.createElement("td");
+        pfp.classList.add("no-b-right");
         const pfp_img = document.createElement("img");
         pfp_img.classList.add("pfp-img");
         pfp_img.src = `/acc/pfp/get/${entry.id}`;
         pfp.appendChild(pfp_img);
-        row.replaceChildren(pfp,...make([["td",{textContent:entry.id}],["td",{textContent:entry.name}],["td",{textContent:entry.level.toString()}],["td",{textContent:(new Date(entry.odate)).toLocaleDateString()}],["td",{textContent:(new Date(entry.cdate)).toLocaleDateString()}],["td",{children:[make("span", {children:makeFriendActions(entry.id, entry.friend),classList:["flex-center"]})]}]]));
+        row.replaceChildren(pfp,...make([["td",{textContent:entry.id,"classList":["no-b-left"]}],["td",{textContent:entry.name}],["td",{textContent:entry.level.toString()}],["td",{textContent:(new Date(entry.odate)).toLocaleDateString()}],["td",{textContent:(new Date(entry.cdate)).toLocaleDateString()}],["td",{children:[make("span", {children:makeFriendActions(entry.id, entry.friend),classList:["flex-center"]})]}]]));
         rows.push(row);
     }
     listTable.replaceChildren(...rows);
